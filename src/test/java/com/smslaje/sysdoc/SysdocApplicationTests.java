8613f15cbc5d3c5294846aeb1d9cdf4ba9fa7e11
carlosandre-dev <andre.tjlba@gmail.com>
2022-12-20 18:08:22 -0300

    Starting Project for document control

---
+++ src/test/java/com/smslaje/sysdoc/SysdocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smslaje.sysdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysdocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
